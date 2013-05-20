fec414f5eaf20c365eeeb1d6c6d99ce4fe1cfc60
Christophe Grand <christophe@cgrand.net>
2013-05-20 18:10:49 +0200

    don't allow bots to excess the max processing time

--- src/tron/bots.clj
+++ src/tron/bots.clj
@@ -6,6 +6,17 @@
   [look {[x y] :pos}]
   {:pos [(inc x) y]})
 
+(defn right [[x y]]
+  [(inc x) y])
+
+(defn down [[x y]]
+  [x (inc y)])
+
+(defn down-or-right
+  [look {pos :pos}]
+  (if (look (down pos))
+    {:pos (right pos)}
+    {:pos (down pos)}))
 
 ;; launch two buzzes
 #_(doseq [s [buzz buzz]]
