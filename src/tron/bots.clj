5dcea7541fa97caefd0d68efbb6a2b413574a73c
Christophe Grand <christophe@cgrand.net>
2013-06-26 13:50:31 +0100

    stub fns with post conds

--- src/tron/bots.clj
+++ src/tron/bots.clj
@@ -33,5 +33,34 @@
     {:pos (right pos)}
     {:pos (down pos)}))
 
+(def dirs #{[0 1] [0 -1] [-1 0] [1 0]})
+(defn addv [a b] (mapv + a b))
+(defn subv [a b] (mapv - a b))
+(defn are-neighbours? [a b]
+  (contains? dirs (subv a b)))
+
+(defn neighbours
+  "returns a seq or coll of the 4 neighbours of the current
+   pos, irrespective of whether these positions are valid o
+   free."
+  [pos]
+  {:post [(every? are-neighbours? %)]}
+  :TODO)
+
+(defn valid-neighbours
+  "returns a seq or coll of the neighbours of the current
+   pos that are free to go (ie look returns nil for those)."
+  [look pos]
+  {:post [(not-any? look %) (every? are-neighbours? %)]}
+  :TODO)
+
+(defn valid-path
+  "returns a seq of the all the next positions that are free
+   to go in the given direction until one occupied one is
+   reached."
+  [look pos dir]
+  {:post [(not-any? look %) (every? (fn [[a b]] (= dir (subv b a))) (partition 2 1 %))]}
+  :TODO)
+
 ;; launch bots
 (tron/run buzz down-or-right)
