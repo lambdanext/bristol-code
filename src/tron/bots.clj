910b09dcbff9c6f418d98f85ba1e68ddf5fd424e
Christophe Grand <christophe@cgrand.net>
2013-06-26 09:55:48 +0100

    add mock look fns

--- src/tron/bots.clj
+++ src/tron/bots.clj
@@ -1,6 +1,21 @@
 (ns tron.bots
   (:require [tron.core :as tron]))
 
+(defn empty-look 
+  "A mock look function which just checks for the arena
+   boundaries."
+  [pos]
+  (when-not (tron/valid-pos? pos) :wall))
+
+(defn mock-look 
+  "Returns a mock look function which checks for the arena
+   boundaries and the specified occupied positions."
+  [& occupied-poses]
+  (let [occupied-poses (set occupied-poses)]
+    (fn [pos]
+      (or (occupied-poses pos)
+        (when-not (tron/valid-pos? pos) :wall)))))
+
 (defn buzz 
   "To infinity and beyond!"
   [look {[x y] :pos}]
